4b708f758bcc758dd22bbe1783d5d1ba0b38fa8d
AlphaOmegaOLA <61032907+AlphaOmegaOLA@users.noreply.github.com>
2024-02-01 18:11:59 -0600

    Began ShooterIntake Subsystem

---
+++ src/main/java/frc/robot/subsystems/ShooterIntake.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// Using Sparks for our big ol' arm CIM motors
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+
+public class ShooterIntake extends SubsystemBase 
+{
+
+    // Declare actuators, sensors, and other variables here
+
+    /**
+     * Constructs a new ExampleSys.
+     * 
+     * <p>ExampleSys contains the basic framework of a robot subsystem for use in command-based programming.
+     */
+    public ShooterIntake() 
+    {
+        // Initialize and configure actuators and sensors here
+    
+    }
+
+    // This method will be called once per scheduler run
+    @Override
+    public void periodic() 
+    {
+        
+    }
+
+    // Put methods for controlling this subsystem here. Call these from Commands.
+
+}
