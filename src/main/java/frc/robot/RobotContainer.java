10048b625aa4d85371db88b5a37441f7cd10763c
GeospatialPython.com <jlawhead@geospatialpython.com>
2024-02-28 20:08:57 -0600

    Added operator joystick

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -22,7 +22,6 @@ public class RobotContainer
 {
     /* Controllers */
     private final Joystick driver = new Joystick(0);
-
     private final Joystick operator = new Joystick(1);
 
     /* Drive Controls */
